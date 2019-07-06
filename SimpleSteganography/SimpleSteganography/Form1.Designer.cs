d04e7f61a91f8bd2cbe2ad7a177439169380531d
ramesh1999 <45420013+ramesh1999@users.noreply.github.com>
2019-07-06 13:11:56 +0530

    Add files via upload

---
+++ SimpleSteganography/SimpleSteganography/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SimpleSteganography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonEncode = new System.Windows.Forms.Button();
+            this.buttonDecode = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(53, 159);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 1;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // buttonEncode
+            // 
+            this.buttonEncode.Location = new System.Drawing.Point(94, 267);
+            this.buttonEncode.Name = "buttonEncode";
+            this.buttonEncode.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncode.TabIndex = 2;
+            this.buttonEncode.Text = "Encode";
+            this.buttonEncode.UseVisualStyleBackColor = true;
+            // 
+            // buttonDecode
+            // 
+            this.buttonDecode.Location = new System.Drawing.Point(196, 267);
+            this.buttonDecode.Name = "buttonDecode";
+            this.buttonDecode.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecode.TabIndex = 3;
+            this.buttonDecode.Text = "Decode";
+            this.buttonDecode.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(147, 159);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(124, 20);
+            this.textBoxFilePath.TabIndex = 4;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(147, 225);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(124, 20);
+            this.textBoxMessage.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Message";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(53, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 118);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 311);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.buttonDecode);
+            this.Controls.Add(this.buttonEncode);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonEncode;
+        private System.Windows.Forms.Button buttonDecode;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SimpleSteganography/SimpleSteganography/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace SimpleSteganography
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image Files (*.png, *.jpg)| *.png; *.jpg";
+            openDialog.InitialDirectory = @"C:\Users\Ramesh\Desktop";
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = openDialog.FileName.ToString();
+                pictureBox1.ImageLocation = textBoxFilePath.Text;
+            }
+
+        }
+
+
+
+        private void buttonEncode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(textBoxFilePath.Text);
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < textBoxMessage.TextLength)
+                    {
+                        Console.WriteLine("R = [" + i + "][" + j + "] = " + pixel.R);
+                        Console.WriteLine("G = [" + i + "][" + j + "] = " + pixel.G);
+                        Console.WriteLine("G = [" + i + "][" + j + "] = " + pixel.B);
+
+                        char letter = Convert.ToChar(textBoxMessage.Text.Substring(j, 1));
+                        int value = Convert.ToInt32(letter);
+                        Console.WriteLine("letter : " + letter + " value : " + value);
+
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
+                    }
+
+                    if (i == img.Width - 1 && j == img.Height - 1)
+                    {
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, textBoxMessage.TextLength));
+                    }
+
+                }
+            }
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+            saveFile.InitialDirectory = @"C:\Users\Ramesh\Desktop";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = saveFile.FileName.ToString();
+                pictureBox1.ImageLocation = textBoxFilePath.Text;
+
+                img.Save(textBoxFilePath.Text);
+            }
+        }
+
+        private void buttonDecode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(textBoxFilePath.Text);
+            string message = "";
+
+            Color lastpixel = img.GetPixel(img.Width - 1, img.Height - 1);
+            int msgLength = lastpixel.B;
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < msgLength)
+                    {
+                        int value = pixel.B;
+                        char c = Convert.ToChar(value);
+                        string letter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
+
+                        message = message + letter;
+                    }
+                }
+            }
+
+            textBoxMessage.Text = message;
+        }
+
+
+
+
+    }
+
+}
+
+        
+
+
+
+
+
+
+        
+    
+
